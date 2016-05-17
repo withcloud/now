07a50aa169ec52751f84a5ec27221366136dac51
Timo Lins <timo.lins@gmail.com>
2016-05-17 15:10:40 +0200

    First working Release (0.1.0)
    - Refactored Code
    - Support for full API
    - Mocha Tests

--- src/now.js
+++ src/now.js
@@ -1,59 +1,118 @@
 const axios = require('axios');
 
+const ERROR = {
+  MISSING_ID: {
+    code: 'missing_id',
+    message: 'Missing `id` parameter',
+  },
+  MISSING_FILE_ID: {
+    code: 'missing_file_id',
+    message: 'Missing `fileId` parameter',
+  },
+  MISSING_BODY: {
+    code: 'missing_body',
+    message: 'Missing `body` parameter',
+  },
+  MISSING_PACKAGE: {
+    code: 'missing_package',
+    message: 'Missing `package` object in body',
+  },
+};
+
 function Now(token) {
   if (!(this instanceof Now)) return new Now(token);
   this.token = token;
   this.axios = axios.create({
     baseURL: 'https://api.zeit.co/now',
-    timeout: 3000,
+    timeout: 5000,
     headers: { Authorization: `Bearer ${token}` },
   });
 }
 
-const now = Now.prototype;
+Now.prototype = {
+  handleCallback: function handleCallback(callback, err, data) {
+    if (typeof callback === 'function') {
+      callback(err, data);
+    }
+  },
 
-now.handleCallback = function handleCallback(callback, err, data) {
-  if (typeof callback === 'function') {
-    callback(err, data);
-  }
+  handleError: function handleCallback(err, callback) {
+    return new Promise((resolve, reject) => {
+      reject(err);
+      this.handleCallback(callback, err);
+    });
+  },
+
+  handleRequest: function handleRequest(config, callback, selector) {
+    return new Promise((resolve, reject) => {
+      this.axios.request(config)
+        .then((res) => {
+          const data = selector ? res.data[selector] : res.data;
+          resolve(data);
+          this.handleCallback(callback, undefined, data);
+        })
+        .catch((err) => {
+          const errData = err.data.err ? err.data.err : err.data;
+          reject(errData);
+          this.handleCallback(callback, errData);
+        });
+    });
+  },
+
+  getDeployments: function getDeployments(callback) {
+    return this.handleRequest({
+      url: '/deployments',
+      method: 'get',
+    }, callback, 'deployments');
+  },
+
+  getDeployment: function getDeployment(id, callback) {
+    if (!id) return this.handleError(ERROR.MISSING_ID, callback);
+
+    return this.handleRequest({
+      url: `/deployments/${id}`,
+      method: 'get',
+    }, callback);
+  },
+
+  createDeployment: function createDeployment(body, callback) {
+    if (!body) return this.handleError(ERROR.MISSING_BODY, callback);
+    if (!body.package) return this.handleError(ERROR.MISSING_PACKAGE, callback);
+
+    return this.handleRequest({
+      url: '/deployments',
+      method: 'post',
+      data: body,
+    }, callback);
+  },
+
+  deleteDeployment: function deleteDeployment(id, callback) {
+    if (!id) return this.handleError(ERROR.MISSING_ID, callback);
+
+    return this.handleRequest({
+      url: `/deployments/${id}`,
+      method: 'delete',
+    }, callback);
+  },
+
+  getFiles: function getFiles(id, callback) {
+    if (!id) return this.handleError(ERROR.MISSING_ID, callback);
+
+    return this.handleRequest({
+      url: `/deployments/${id}/files`,
+      method: 'get',
+    }, callback);
+  },
+
+  getFile: function getFile(id, fileId, callback) {
+    if (!id) return this.handleError(ERROR.MISSING_ID, callback);
+    if (!fileId) return this.handleError(ERROR.MISSING_FILE_ID, callback);
+
+    return this.handleRequest({
+      url: `/deployments/${id}/files/${fileId}`,
+      method: 'get',
+    }, callback);
+  },
 };
-
-now.handleRequest = function handleRequest(config, callback, selector) {
-  return new Promise((resolve, reject) => {
-    this.axios.request(config)
-      .then((res) => {
-        const data = selector ? res.data[selector] : res.data;
-
-        resolve(data);
-        this.handleCallback(callback, undefined, data);
-      })
-      .catch((err) => {
-        const errData = err.data.err ? err.data.err : err.data;
-
-        reject(errData);
-        this.handleCallback(callback, errData);
-      });
-  });
-};
-
-now.deployments = function deployments(callback) {
-  return this.handleRequest({ url: '/deployments', method: 'get' }, callback, 'deployments');
-};
-
-now.deployment = function deployment(id, callback) {
-  return this.handleRequest({ url: `/deployments/${id}`, method: 'get' }, callback);
-};
-
-now.files = function files(id, callback) {
-  return this.handleRequest({ url: `/deployments/${id}/files`, method: 'get' }, callback);
-};
-
-now.file = function file(id, fileId, callback) {
-  return this.handleRequest({ url: `/deployments/${id}/files/${fileId}`, method: 'get' }, callback);
-};
-
-// DELETE /now/deployments/:id
-
-// POST /now/deployments
 
 module.exports = Now;
--- test/index.js
+++ test/index.js
@@ -1,11 +1,106 @@
 const Now = require('../lib/now');
 
+const chai = require('chai');
+chai.should();
+
 const TOKEN = process.env.TEST_NOW_TOKEN;
 
-const now = Now(TOKEN);
+if (!TOKEN) {
+  throw new Error('now token not provided');
+}
 
-now.deployments().then((data) => {
-  console.log(data);
-}).catch((err) => {
-  console.log(err);
+describe('Now', function tests() {
+  this.timeout(10000);
+
+  const now = new Now(TOKEN);
+
+  let instanceId;
+  let fileId;
+
+  it('should create deployment', (done) => {
+    now.createDeployment({
+      package: {
+        name: 'test-deployment',
+        scripts: {
+          start: 'node index',
+        },
+      },
+      'index.js': 'console.log("Unit Test!")',
+    })
+    .then((data) => {
+      data.should.be.an('object');
+      instanceId = data.uid;
+      done();
+    })
+    .catch((err) => {
+      done(new Error(err.message));
+    });
+  });
+
+  it('should retrieve deployments', (done) => {
+    now.getDeployments()
+    .then((data) => {
+      data.should.be.an('array');
+      done();
+    }).catch((err) => {
+      done(new Error(err.message));
+    });
+  });
+
+  it('should retrieve deployments via callback', (done) => {
+    now.getDeployments((err, data) => {
+      if (err) return done(new Error(err.message));
+      data.should.be.an('array');
+      return done();
+    });
+  });
+
+  it('should retrieve single deployment', (done) => {
+    now.getDeployment(instanceId)
+    .then((data) => {
+      data.should.be.an('object');
+      done();
+    }).catch((err) => {
+      done(new Error(err.message));
+    });
+  });
+
+  it('should retrieve file list from deployment', (done) => {
+    now.getFiles(instanceId)
+    .then((data) => {
+      data.should.be.an('array');
+      const file = data[0];
+      file.type.should.be.a('string');
+
+      fileId = file.uid;
+
+      done();
+    }).catch((err) => {
+      done(new Error(err.message));
+    });
+  });
+
+  it('should retrieve file content from deployment', (done) => {
+    now.getFile(instanceId, fileId)
+    .then((data) => {
+      data.should.be.a('string');
+
+      done();
+    }).catch((err) => {
+      done(new Error(err.message));
+    });
+  });
+
+  it('should remove deployment', (done) => {
+    now.deleteDeployment(instanceId)
+    .then((data) => {
+      data.should.be.an('object');
+      data.uid.should.equal(instanceId);
+      data.state.should.equal('DELETED');
+
+      done();
+    }).catch((err) => {
+      done(new Error(err.message));
+    });
+  });
 });
